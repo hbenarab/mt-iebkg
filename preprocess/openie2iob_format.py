5e441571a468eadbdb7c7ad7d1b85005fd7a79d9
hbenarab <ben.arab.heni@gmail.com>
2015-08-08 20:03:41 +0200

    file converting the openIE output to the IOB format created

--- preprocess/openie2iob_format.py
+++ preprocess/openie2iob_format.py
@@ -1,1 +1,22 @@
 __author__ = 'heni'
+
+import os
+
+# os.getcwd()
+# f=open('../qa-jbt/data/openie/Paris.openie','r')
+# lines=f.readlines()
+# print(lines[0])
+# print(lines[1])
+# print(lines[2])
+
+
+def convert_openie2iob(article_name):
+    article=open(os.path.join('../qa-jbt/data/openie',article_name))
+    lines=article.readlines()[1:] # the first line containing the date of the file's creation is ignored
+    iob_file=open('../data/iob/'+article_name+'.iob','w')
+    for i in range(0,len(lines)):
+        line_elements=lines[i].split('\t')[2:]
+
+
+    return
+
