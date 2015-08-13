578221fc5a96b76ec3750896e8c305e44c189b7a
hbenarab <ben.arab.heni@gmail.com>
2015-08-13 16:15:41 +0200

    Errors fixing  finished. Testing ongoing Adding metrics from G.Mesnil work (.prl)

--- process.py
+++ process.py
@@ -6,12 +6,13 @@ import numpy
 import datetime
 import math
 import time
-import sys
+
 from preprocess.wordemb import WordEmbeddings, create_word_index
 from preprocess.preprocess import get_iob
 from rnn.elman_model import Elman
 from preprocess.labeledText import LabeledText
 from utils.tools import shuffle, minibatch, contextwin
+from is13.metrics.accuracy import conlleval
 
 
 # this function aims to train a RNN
@@ -98,7 +99,7 @@ def run_process(articles):
                 'nhidden': 100,  # number of hidden units
                 'seed': 345,
                 'emb_dimension': 100,  # dimension of word embedding
-                'nepochs': 10}
+                'nepochs': 1}
     indices = {
         'wordIndex': WordEmbeddings(),
         'labelIndex': WordEmbeddings()
@@ -216,10 +217,13 @@ def run_process(articles):
             predictions_test = [map(lambda x: index2label[x],
                                     rnn.classify(numpy.asarray(contextwin(x, settings['win'])).astype('int32')))
                                 for x in [[word2index[word] for word in sentence] for sentence in test_sentences]]
-            for sen, pred, lab in zip(test_sentences, predictions_test, test_labels):
-                print(sen)
-                print('Pred: %s' % pred)
-                print('Orig: %s' % lab)
+            groundTruth_test=[ map(lambda x: index2label[x], y) for y in test_labels ]
+            words_test = [ map(lambda x: index2word[x], w)
+                           for w in [[word2index[word] for word in sentence] for sentence in test_sentences]]
+
+            # evaluation // compute the accuracy using conlleval.pl
+
+
 
             correctGuesses_list = [[1 if pred_val == exp_val else 0 for pred_val, exp_val in zip(pred, exp)]
                                    for pred, exp in zip(predictions_test, test_labels)]
